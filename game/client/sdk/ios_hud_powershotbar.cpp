1e889bc5f7c9a396020b871facb1d7d88d19d976
Roman Dittert <romd86@gmail.com>
2016-08-01 05:41:28 +0200

    Rework charged shot and stamina bar visuals

--- game/client/sdk/ios_hud_powershotbar.cpp
+++ game/client/sdk/ios_hud_powershotbar.cpp
@@ -27,8 +27,8 @@
 // memdbgon must be the last include file in a .cpp file!!!
 #include "tier0/memdbgon.h"
 
-enum { STAMINABAR_HMARGIN = 30, STAMINABAR_VMARGIN = 30, STAMINABAR_WIDTH = 200, STAMINABAR_HEIGHT = 12, STAMINABAR_BORDER = 2 };
-enum { SHOTBAR_VMARGIN = 30, SHOTBAR_WIDTH = 200, SHOTBAR_HEIGHT = 12, SHOTBAR_BORDER = 2 };
+enum { STAMINABAR_VMARGIN = 30, STAMINABAR_WIDTH = 200, STAMINABAR_HEIGHT = 6, STAMINABAR_BORDER = 2 };
+enum { SHOTBAR_VMARGIN = 44, SHOTBAR_WIDTH = 200, SHOTBAR_HEIGHT = 6, SHOTBAR_BORDER = 2 };
 
 class CHudChargedshotBar : public CHudElement, public vgui::Panel
 {
@@ -137,29 +137,52 @@ void CHudChargedshotBar::Paint()
 
 	float stamina = pPlayer->m_Shared.GetStamina() / 100.0f;
 	
-	Color staminaFgColor = g_ColorGreen;
-	Color staminaBgColor = Color(0, 0, 0, 255);
+	Color staminaFgColor = g_ColorOrange;
+	Color staminaBorderColor = Color(0, 0, 0, 255);
+	Color staminaBgColor = Color(33, 33, 33, 255);
 
-	// Draw stamina bar back
+	// Draw shot bar border
+	surface()->DrawSetColor(staminaBorderColor);
+	surface()->DrawFilledRect(
+		ScreenWidth() / 2 - STAMINABAR_WIDTH / 2 - STAMINABAR_BORDER,
+		ScreenHeight() - STAMINABAR_VMARGIN - STAMINABAR_HEIGHT - 2 * STAMINABAR_BORDER,
+		ScreenWidth() / 2 - STAMINABAR_WIDTH / 2 - STAMINABAR_BORDER + STAMINABAR_WIDTH + 2 * STAMINABAR_BORDER,
+		ScreenHeight() - STAMINABAR_VMARGIN - STAMINABAR_HEIGHT - 2 * STAMINABAR_BORDER + STAMINABAR_HEIGHT + 2 * STAMINABAR_BORDER
+	);
+
+	// Draw shot bar background
 	surface()->DrawSetColor(staminaBgColor);
 	surface()->DrawFilledRect(
-		ScreenWidth() - STAMINABAR_HMARGIN - STAMINABAR_WIDTH - 2 * STAMINABAR_BORDER,
-		STAMINABAR_VMARGIN,
-		ScreenWidth() - STAMINABAR_HMARGIN - STAMINABAR_WIDTH - 2 * STAMINABAR_BORDER + STAMINABAR_WIDTH + 2 * STAMINABAR_BORDER,
-		STAMINABAR_VMARGIN + STAMINABAR_HEIGHT + 2 * STAMINABAR_BORDER
+		ScreenWidth() / 2 - STAMINABAR_WIDTH / 2,
+		ScreenHeight() - STAMINABAR_VMARGIN - STAMINABAR_HEIGHT - STAMINABAR_BORDER,
+		ScreenWidth() / 2 + STAMINABAR_WIDTH / 2,
+		ScreenHeight() - STAMINABAR_VMARGIN - STAMINABAR_HEIGHT - STAMINABAR_BORDER + STAMINABAR_HEIGHT
 	);
 
-	// Draw stamina bar front
+	// Draw shot bar front
 	surface()->DrawSetColor(staminaFgColor);
 	surface()->DrawFilledRect(
-		ScreenWidth() - STAMINABAR_HMARGIN - STAMINABAR_WIDTH - STAMINABAR_BORDER,
-		STAMINABAR_VMARGIN + STAMINABAR_BORDER,
-		ScreenWidth() - STAMINABAR_HMARGIN - STAMINABAR_WIDTH - STAMINABAR_BORDER + stamina * STAMINABAR_WIDTH,
-		STAMINABAR_VMARGIN + STAMINABAR_BORDER + STAMINABAR_HEIGHT
+		ScreenWidth() / 2 - STAMINABAR_WIDTH / 2,
+		ScreenHeight() - STAMINABAR_VMARGIN - STAMINABAR_HEIGHT - STAMINABAR_BORDER,
+		ScreenWidth() / 2 - STAMINABAR_WIDTH / 2 + stamina * STAMINABAR_WIDTH,
+		ScreenHeight() - STAMINABAR_VMARGIN - STAMINABAR_HEIGHT - STAMINABAR_BORDER + STAMINABAR_HEIGHT
 	);
 
+	surface()->DrawSetColor(staminaBorderColor);
+
+	for (int i = 1; i <= 3; i++)
+	{
+		surface()->DrawFilledRect(
+			ScreenWidth() / 2 - STAMINABAR_WIDTH / 2 + i * 0.25f * STAMINABAR_WIDTH - 1,
+			ScreenHeight() - STAMINABAR_VMARGIN - STAMINABAR_HEIGHT - STAMINABAR_BORDER,
+			ScreenWidth() / 2 - STAMINABAR_WIDTH / 2 + i * 0.25f * STAMINABAR_WIDTH + 1,
+			ScreenHeight() - STAMINABAR_VMARGIN - STAMINABAR_HEIGHT - STAMINABAR_BORDER + STAMINABAR_HEIGHT
+		);
+	}
+
 	Color shotFgColor;
-	Color shotBgColor = Color(0, 0, 0, 255);
+	Color shotBorderColor = Color(0, 0, 0, 255);
+	Color shotBgColor = Color(33, 33, 33, 255);
 	bool shotBlocked = true;
 
 	if (pPlayer->GetFlags() & FL_REMOTECONTROLLED)
@@ -170,29 +193,47 @@ void CHudChargedshotBar::Paint()
 		shotFgColor = g_ColorOrange;
 	else
 	{
-		shotFgColor = g_ColorBlue;
+		shotFgColor = g_ColorLightBlue;
 		shotBlocked = false;
 	}
 
-	if (pPlayer->m_Shared.m_bDoChargedShot || pPlayer->m_Shared.m_bIsShotCharging || shotBlocked)
+	float shotStrength = shotBlocked ? 1.0f : pPlayer->GetChargedShotStrength();
+
+	// Draw shot bar border
+	surface()->DrawSetColor(shotBorderColor);
+	surface()->DrawFilledRect(
+		ScreenWidth() / 2 - SHOTBAR_WIDTH / 2 - SHOTBAR_BORDER,
+		ScreenHeight() - SHOTBAR_VMARGIN - SHOTBAR_HEIGHT - 2 * SHOTBAR_BORDER,
+		ScreenWidth() / 2 - SHOTBAR_WIDTH / 2 - SHOTBAR_BORDER + SHOTBAR_WIDTH + 2 * SHOTBAR_BORDER,
+		ScreenHeight() - SHOTBAR_VMARGIN - SHOTBAR_HEIGHT - 2 * SHOTBAR_BORDER + SHOTBAR_HEIGHT + 2 * SHOTBAR_BORDER
+	);
+
+	// Draw shot bar background
+	surface()->DrawSetColor(shotBgColor);
+	surface()->DrawFilledRect(
+		ScreenWidth() / 2 - SHOTBAR_WIDTH / 2,
+		ScreenHeight() - SHOTBAR_VMARGIN - SHOTBAR_HEIGHT - SHOTBAR_BORDER,
+		ScreenWidth() / 2 + SHOTBAR_WIDTH / 2,
+		ScreenHeight() - SHOTBAR_VMARGIN - SHOTBAR_HEIGHT - SHOTBAR_BORDER + SHOTBAR_HEIGHT
+	);
+
+	// Draw shot bar front
+	surface()->DrawSetColor(shotFgColor);
+	surface()->DrawFilledRect(
+		ScreenWidth() / 2 - SHOTBAR_WIDTH / 2,
+		ScreenHeight() - SHOTBAR_VMARGIN - SHOTBAR_HEIGHT - SHOTBAR_BORDER,
+		ScreenWidth() / 2 - SHOTBAR_WIDTH / 2 + shotStrength * SHOTBAR_WIDTH,
+		ScreenHeight() - SHOTBAR_VMARGIN - SHOTBAR_HEIGHT - SHOTBAR_BORDER + SHOTBAR_HEIGHT
+	);
+
+	surface()->DrawSetColor(shotBorderColor);
+
+	for (int i = 1; i <= 3; i++)
 	{
-		float shotStrength = shotBlocked ? 1.0f : pPlayer->GetChargedShotStrength();
-
-		// Draw shot bar back
-		surface()->DrawSetColor(shotBgColor);
 		surface()->DrawFilledRect(
-			ScreenWidth() / 2 - SHOTBAR_WIDTH / 2 - SHOTBAR_BORDER,
-			ScreenHeight() - SHOTBAR_VMARGIN - SHOTBAR_HEIGHT - 2 * SHOTBAR_BORDER,
-			ScreenWidth() / 2 - SHOTBAR_WIDTH / 2 - SHOTBAR_BORDER + SHOTBAR_WIDTH + 2 * SHOTBAR_BORDER,
-			ScreenHeight() - SHOTBAR_VMARGIN - SHOTBAR_HEIGHT - 2 * SHOTBAR_BORDER + SHOTBAR_HEIGHT + 2 * SHOTBAR_BORDER
-		);
-
-		// Draw shot bar front
-		surface()->DrawSetColor(shotFgColor);
-		surface()->DrawFilledRect(
-			ScreenWidth() / 2 - SHOTBAR_WIDTH / 2,
+			ScreenWidth() / 2 - SHOTBAR_WIDTH / 2 + i * 0.25f * SHOTBAR_WIDTH - 1,
 			ScreenHeight() - SHOTBAR_VMARGIN - SHOTBAR_HEIGHT - SHOTBAR_BORDER,
-			ScreenWidth() / 2 - SHOTBAR_WIDTH / 2 + shotStrength * SHOTBAR_WIDTH,
+			ScreenWidth() / 2 - SHOTBAR_WIDTH / 2 + i * 0.25f * SHOTBAR_WIDTH + 1,
 			ScreenHeight() - SHOTBAR_VMARGIN - SHOTBAR_HEIGHT - SHOTBAR_BORDER + SHOTBAR_HEIGHT
 		);
 	}
